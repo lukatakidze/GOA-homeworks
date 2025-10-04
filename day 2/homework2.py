22daa2c5fe6d93a6c4c2309cea8514f91f95f473
lukatakidze <luka.takidze1234@gmail.com>
2025-10-04 16:17:00 +0400

    third commit

---
+++ day 2/homework2.py
@@ -0,0 +1,23 @@
+
+name = "luka"
+
+age = 17 
+
+height = 1.77 
+
+print(name,age,height)
+
+     
+int = 23
+print(int + 7)
+
+#numberthatisstoredinvariable ცვლადის სახელი უფრო გასაგები რომ გახდეს უნდა გამოვიყენოთ სიმბოლო _ 
+#number_that_is_stored_in_variable
+
+#name = "nino"
+#print(name + 3) მოცემული კოდი იმიტომ გამოიტანს ერორს რომ ჩვენ ერთმანეთს ვერდავუმატებთ სახელს და რიცხვს
+
+firstname = "luka"
+lastname = "takidze"
+
+print(firstname + lastname)
