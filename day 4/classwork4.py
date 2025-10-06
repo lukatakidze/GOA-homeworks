41b9a192856107e014bc63885df76157b37ab469
lukatakidze <luka.takidze1234@gmail.com>
2025-10-06 19:37:49 +0400

    fifth commit

---
+++ day 4/classwork4.py
@@ -0,0 +1,10 @@
+
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+
+print(num1 + num2 )
+print(num1 - num2 )
+print(num1 * num2 )
+print(num1 / num2 )
+
+
---
+++ day 4/homework4.py
@@ -0,0 +1,31 @@
+
+name = "Nino"
+print(name + str(3))
+
+#converter დანიშნულება არის რომ ერთი მნიშვნელობა გადააქციოს სხვა მნიშვნელობად
+
+#debugging ვიყენებთ ბაგის აღმოსაჩენად და ამ ბაგის გამოსწორებისთვის რაც ძალიან მნიშვნელოვანია
+
+#True და False მნიშვნელობები მიეკუთნება boolean ტიპს
+
+firstname_lastname = "luka takidze"
+#რთული გზა 
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+print(firstname_lastname)
+#მარტივი გზა 
+print(firstname_lastname * 17 )
