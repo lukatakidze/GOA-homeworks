2b94e7f955d399ef6f23e6af7ae5e422bb420d1d
lukatakidze <luka.takidze1234@gmail.com>
2025-10-11 19:21:46 +0400

    sixth commit

---
+++ day 5/classwork5.py
@@ -0,0 +1,7 @@
+
+age = 17
+if age >= 18:
+ print("adult")
+
+else:
+  print("kid")
---
+++ day 5/homework5.py
@@ -0,0 +1,25 @@
+
+for i in range(18):
+  print("lukatakidze")
+
+for x in range(1, 11):
+ print(x)
+
+
+num1 = int(input("enter number:"))
+
+if num1 % 2  == 0:
+    print("your number is even ")
+else:
+    print("your number is odd")
+
+
+for y in range(1, 21):
+ 
+  if  y % 2 == 0:
+     print("this number is even")
+else:
+     print("this number is odd")
+
+
+
